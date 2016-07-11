fec34b3a366891d7a6693e74e7acbdbd62f8a3be
eric-galdino <ericgaldino0@gmail.com>
2016-07-11 09:01:29 -0300

    Login / Signup
    Login and Signup - XML

--- CommunicatingFragments-Bundle/app/src/main/java/com/eric/communicatingfragments_bundle/MyConnector.java
+++
@@ -1,8 +0,0 @@
-package com.eric.communicatingfragments_bundle;
-
-/**
- * Created by Eric on 28/06/2016.
- */
-public interface MyConnector{
-    public void getValueFromFragmentUsingInterface(String v);
-}
--- CommunicationFragment/app/src/main/java/com/eric/communicationfragment/Communicator.java
+++
@@ -1,9 +0,0 @@
-package com.eric.communicationfragment;
-
-/**
- * Created by Eric on 08/06/2016.
- */
-public interface Communicator {
-
-    public void responder(String data);
-}
